fb3633dcb405c77995f2988dcf8ca71f896e15fd
liuxiaokun <liuxiaokun@shengchaotec.com>
2022-09-25 22:00:18 +0800

    增加mybatis plus 拦截器

---
+++ src/main/java/com/lxk/mgr/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.lxk.mgr.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        log.info("mybatisPlusInterceptor...");
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
